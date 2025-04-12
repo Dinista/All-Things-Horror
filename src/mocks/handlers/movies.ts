7847f844897b3fd159bfa351b4be6ba80d6929b7
Dinista <ra110752@uem.br>
2025-04-12 14:18:44 -0300

    var correction

--- src/mocks/handlers/movies.ts
+++ src/mocks/handlers/movies.ts
@@ -1,10 +1,10 @@
 import { Movie, ApiResponse } from '@/types/api/types';
-import { movies } from '@/mocks/data/mockMovies';
+import { mockMovies } from '@/mocks/data/mockMovies';
 
 export const fetchMovies = async (): Promise<ApiResponse<Movie[]>> => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve({ success: true, data: movies });
+            resolve({ success: true, data: mockMovies });
         }, 500);
     });
 };
@@ -13,7 +13,7 @@ export const fetchMovies = async (): Promise<ApiResponse<Movie[]>> => {
 export const fetchMovieById = async (id: number): Promise<ApiResponse<Movie>> => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const movie = movies.find((m) => m.id === id);
+            const movie = mockMovies.find((m) => m.id === id);
             if (movie) {
                 resolve({ success: true, data: movie });
             } else {
@@ -27,7 +27,7 @@ export const fetchMovieById = async (id: number): Promise<ApiResponse<Movie>> =>
 export const addMovie = async (newMovie: Movie): Promise<ApiResponse<Movie>> => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            movies.push(newMovie);
+            mockMovies.push(newMovie);
             resolve({ success: true, data: newMovie });
         }, 500);
     });
@@ -37,9 +37,9 @@ export const addMovie = async (newMovie: Movie): Promise<ApiResponse<Movie>> => 
 export const updateMovie = async (updatedMovie: Movie): Promise<ApiResponse<Movie>> => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const movieIndex = movies.findIndex((m) => m.id === updatedMovie.id);
+            const movieIndex = mockMovies.findIndex((m) => m.id === updatedMovie.id);
             if (movieIndex !== -1) {
-                movies[movieIndex] = updatedMovie;
+                mockMovies[movieIndex] = updatedMovie;
                 resolve({ success: true, data: updatedMovie });
             } else {
                 resolve({ success: false, error: "Movie not found" });
@@ -52,9 +52,9 @@ export const updateMovie = async (updatedMovie: Movie): Promise<ApiResponse<Movi
 export const deleteMovie = async (id: number): Promise<ApiResponse<null>> => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            const movieIndex = movies.findIndex((m) => m.id === id);
+            const movieIndex = mockMovies.findIndex((m) => m.id === id);
             if (movieIndex !== -1) {
-                movies.splice(movieIndex, 1);
+                mockMovies.splice(movieIndex, 1);
                 resolve({ success: true });
             } else {
                 resolve({ success: false, error: "Movie not found" });
