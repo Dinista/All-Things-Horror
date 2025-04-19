467db8cbf3e664c0cd42bc791080acec051f7055
Dinista <ra110752@uem.br>
2025-04-18 23:59:19 -0300

    still

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function contact() {
@@ -5,6 +6,14 @@ export default function contact() {
         <section>
             <h1>How it's done!</h1>
             <h2>follow closely</h2>
+            <div>
+                <Link href={''}>
+                    Check out the code in Github
+                </Link>
+                <Link href={''}>
+                    Check out the code in Github
+                </Link>
+            </div>
         </section>
     )
 }
