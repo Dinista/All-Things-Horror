c845e324b22bcd96ca03bde71719c0fea052deec
Dinista <ra110752@uem.br>
2025-05-13 23:09:53 -0300

    updatye

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
         <section className='w-full flex justify-center items-center flex-col gap-6 mt-32 mb-32'>
             <div className='flex flex-col gap-4 justify-center items-center'>
                 <h1 className='font-genera text-defaultRed uppercase text-2xl'>How it's done!</h1>
-                <h2 className='uppercase font-neoplanta text-7xl'>follow closely</h2>
+                <h2 className='uppercase font-neoplanta text-7xl'>Follow closely</h2>
                 <p className='font-argentum'>Check out the code in Github Check out the code in Github</p>
             </div>
             <div className='flex flex-col uppercase items-center text-center font-[600] w-[500px] gap-4 mt-8'>
