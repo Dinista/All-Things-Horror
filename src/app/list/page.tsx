2c1d9b6308590aefa83a7aa35c97d5b70fd3b16d
Dinista <ra110752@uem.br>
2025-05-20 10:34:49 -0300

    list working

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,32 +1,31 @@
-import { ListMenu } from '@/components/listMenu/listMenu'
-import { ReviewCard } from '@/components/reviewCard/reviewCard'
-import { PageTemplate } from '@/templates/PageTemplate'
-import { mockMovies } from '@/mocks/data/mockMovies'
-
-import React from 'react'
+import { ListMenu } from "@/components/listMenu/listMenu";
+import { ReviewCard } from "@/components/reviewCard/reviewCard";
+import { PageTemplate } from "@/templates/PageTemplate";
+import { mockMovies } from "@/mocks/data/mockMovies";
+import React from "react";
 
 export default function page() {
   return (
     <PageTemplate>
-      <div>
-        <nav className='flex flex-col gap-4'>
-          <h1 className='uppercase text-2xl font-genera text-defaultRed'>
+      <div className="container mx-auto px-4">
+        <nav className="mb-8">
+          <h1 className="uppercase text-2xl font-genera text-defaultRed mb-4">
             Review List
           </h1>
           <ListMenu />
         </nav>
-        <section className="flex flex-wrap gap-6 mt-6 justify-start">
+        <section className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-6">
           {mockMovies.map((movie, index) => (
             <ReviewCard
-              key={index}
+              key={movie.id || index}
               title={movie.title}
               description={movie.synopsis}
               imageUrl={movie.posterUrl}
-              href={''}
+              href={`/reviews/${movie.id || index}`}
             />
           ))}
         </section>
       </div>
     </PageTemplate>
-  )
+  );
 }
--- src/components/listMenu/listMenu.tsx
+++ src/components/listMenu/listMenu.tsx
@@ -1,43 +1,47 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 
 const filtros = [
-    "Todos",
-    "Slasher",
-    "Supernatural",
-    "Zombies",
-    "Psychological",
-    "Vampire",
+  "Todos",
+  "Slasher",
+  "Supernatural",
+  "Zombies",
+  "Psychological",
+  "Vampire",
 ];
 
 export function ListMenu({
-    onFiltroSelecionado,
+  onFiltroSelecionado,
 }: {
-    onFiltroSelecionado?: (filtro: string) => void;
+  onFiltroSelecionado?: (filtro: string) => void;
 }) {
-    const [filtroSelecionado, setFiltroSelecionado] = useState("Todos");
+  const [filtroSelecionado, setFiltroSelecionado] = useState("Todos");
 
-    const handleFiltroClick = (filtro: string) => {
-        setFiltroSelecionado(filtro);
-        onFiltroSelecionado?.(filtro);
-    };
+  const handleFiltroClick = (filtro: string) => {
+    setFiltroSelecionado(filtro);
+    onFiltroSelecionado?.(filtro);
+  };
 
-    return (
-        <div className="flex gap-2 flex-wrap font-argentum font-[500] text-sm">
-            {filtros.map((filtro) => {
-                const isAtivo = filtro === filtroSelecionado;
-                return (
-                    <button
-                        key={filtro}
-                        onClick={() => handleFiltroClick(filtro)}
-                        className={`border-2 border-white/10 px-4 py-1 rounded-md transition-colors duration-200
-                        ${isAtivo ? "bg-defaultRed text-white font-[600] border-white/5" : "text-white/45 hover:bg-white/15"}`}
-                    >
-                        {filtro}
-                    </button>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="flex gap-2 font-argentum font-[500] text-sm">
+      {filtros.map((filtro) => {
+        const isAtivo = filtro === filtroSelecionado;
+        return (
+          <button
+            key={filtro}
+            onClick={() => handleFiltroClick(filtro)}
+            className={`border-2 border-white/10 px-4 py-1 rounded-md transition-colors duration-200
+                        ${
+                          isAtivo
+                            ? "bg-defaultRed text-white font-[600] border-white/5"
+                            : "text-white/45 hover:bg-white/15"
+                        }`}
+          >
+            {filtro}
+          </button>
+        );
+      })}
+    </div>
+  );
 }
