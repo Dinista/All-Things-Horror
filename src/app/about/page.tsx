1ce61ea1da3516528148ce5ef72ea1a08d0ff454
Dinista <ra110752@uem.br>
2025-05-20 11:57:18 -0300

    about us page start

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { PageTemplate } from "@/templates/PageTemplate";
+import React from "react";
+
+export default function page() {
+  return (
+    <PageTemplate>
+      <div className="container mx-auto px-4"></div>
+    </PageTemplate>
+  );
+}
