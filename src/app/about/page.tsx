85c29bb5cda706477e02475d68de88bb12fe54f4
Dinista <ra110752@uem.br>
2025-05-22 17:41:51 -0300

    All eyez on us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,35 @@
 import { PageTemplate } from "@/templates/PageTemplate";
 import React from "react";
+import Image from "next/image";
 
 export default function page() {
   return (
     <PageTemplate>
-      <div className="container mx-auto px-4"></div>
+      <div className="container mx-auto px-4">
+        <section className="flex gap-8 p-4">
+          <div className="w-1/3">
+            <Image
+              src={"/horror_characters.png"}
+              alt="Horror Characters"
+              width={400}
+              height={400}
+              quality={100}
+            />
+          </div>
+          <div className="flex flex-col gap-8 w-1/2">
+            <h1 className="text-4xl font-neoplanta uppercase">
+              All eyez on YOU
+            </h1>
+            <p className="text-gray-400 font-argentum">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+              pariatur eum eius quod ducimus rerum explicabo eveniet minima
+              quisquam, similique debitis? Veniam eos voluptate sequi doloremque
+              numquam est tenetur consequatur!
+            </p>
+            <p>Thank You! You&apos;re the responsable for this project</p>
+          </div>
+        </section>
+      </div>
     </PageTemplate>
   );
 }
--- src/components/footer/footerContent.tsx
+++ src/components/footer/footerContent.tsx
@@ -1,15 +1,23 @@
 import { ReactNode } from "react";
 
 type Props = {
-    header: ReactNode | string;
-    description: string | ReactNode;
+  header: ReactNode | string;
+  description: string | ReactNode;
 };
 
 export const FooterCard = ({ header, description }: Props) => {
-    return (
-        <section className="flex flex-col w-1/5 font-argentum">
-            <h2 className={typeof header === 'string' ? 'font-genera font-bold text-xl text-defaultRed mb-5' : 'mb-2'}>{header}</h2>
-            <p className="text-gray-400">{description}</p>
-        </section>
-    );
+  return (
+    <section className="flex flex-col w-1/5 font-argentum text-sm">
+      <h2
+        className={
+          typeof header === "string"
+            ? "font-genera font-bold text-xl text-defaultRed mb-5"
+            : "mb-2"
+        }
+      >
+        {header}
+      </h2>
+      <p className="text-gray-400">{description}</p>
+    </section>
+  );
 };
